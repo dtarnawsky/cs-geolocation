ed5fe9defc162de74b893106eaa9db801d7a257c
Damian Tarnawsky <damian@ionic.io>
2021-08-23 09:08:41 -0700

    Add Geolocation calls

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  public async getLocation() {    
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log(coordinates);
+  }
+
+  public async requestPermissions() {
+    const permissions = await Geolocation.requestPermissions();
+    console.log(permissions);
+  }
+
+  public async checkPermissions() {
+    const permissions = await Geolocation.checkPermissions();
+    console.log(permissions);
+  }
 
 }
